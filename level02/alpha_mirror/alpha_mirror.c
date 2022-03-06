b936fafd5f6b96c874486f56755671d17ccae081
TUMO Labs Student <tumolabsstudent@TUMOs-iMac.local>
2022-03-06 20:31:41 +0400

    all working, half of level 2

--- level02/alpha_mirror/alpha_mirror.c
+++ level02/alpha_mirror/alpha_mirror.c
@@ -1,32 +1,22 @@
 #include <unistd.h>
-#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
-	int		i;
-	int		j;
-	char	str[26] = "abcdefghijklmnopqrstuvwxyz";
-	
+	int	i;
+
 	i = 0;
-	j = 0;
 	if (argc == 2)
 	{
-		while(argv[1][i] != '\0')
+		while (argv[1][i] != '\0')
 		{
-			if (argv[1][i] >= 'a' && argv[1][i] <= 'm')
-				argv[1][i] = 110 + i;
-		   	else if (argv[1][i] >= 'n' && argv[1][i] <= 'z')
-				argv[1][i] = 97 +  i;
-			else if (argv[1][i] >= 'A' && argv[1][i] <= 'M')
-				argv[1][i] = 78 + i;
-			else if (argv[1][i] >= 'N' && argv[1][i] <= 'Z')
-				argv[1][i] = 90 - i;
+			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+				argv[1][i] = 'M' - (argv[1][i] - 'N');
+			else if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+				argv[1][i] = 'm' - (argv[1][i] - 'n');
 			write(1, &argv[1][i], 1);
-			i++;	
+			i++;
 		}
 	}
 	write(1, "\n", 1);
-	return(0);
-}
-
-
+	return (0);
+}		
